485f26ea7dbdfda79d89745f201fcc72ce38b5c4
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 00:11:19 -0700

    Amazoned.sh
    The shell script for the Amazoned tool.

---
+++ Ucuntu/Amazoned.sh
@@ -0,0 +1,11 @@
+# Start of script
+
+# Experience Amazon Telemetry in pre-Amazon telemetry Ubuntu
+
+ping 108.174.10.10; # The current Amazon IP address
+
+# Just for the lols:
+
+sudo ping 108.174.10.10; # The current Amazon IP address, but with sudo, I don't know if this affects ping
+
+# End of script
