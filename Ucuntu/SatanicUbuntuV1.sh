3281a238253d84fd5054d98f51a7a161c156912a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 12:38:31 -0700

    SatanicUbuntuV1.sh
    An archive of the first version of the Satanic Ubuntu bash script.

---
+++ Ucuntu/SatanicUbuntuV1.sh
@@ -0,0 +1,18 @@
+# Start of script
+
+# Don't know what to do here yet
+
+# I am new to shell, so this should come as pretty script kiddie like.
+
+"""
+File info:
+
+File version: 1 (Tuesday, June 22nd 2020 at 12:38 pm)
+
+Line count (including blank lines): 18
+
+File type: BASH Shell (*.sh)
+
+"""
+
+# End of script
