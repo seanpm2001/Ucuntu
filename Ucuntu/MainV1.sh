41036e507b8d0eea69c70e0dd31e499e8348dc05
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 12:34:40 -0700

    MainV1.sh
    An archive of the first version of the main Shell script.

---
+++ Ucuntu/MainV1.sh
@@ -0,0 +1,34 @@
+# Start of script
+
+""" 
+Info
+This is the main shell script for the Trojan. It controls everything
+"""
+
+# Payloads
+
+# Ubuntu wasn't released before this point
+if date >= 10/20/2004 {
+  run goodbye.sh();
+}
+# Version 6.06 becomes version 6.66
+if date == 06/01/2006 {
+  run satanicUbuntu.sh();
+}
+# Ubuntu release for the Amazon controversy builds
+if date =< 4/16/2012 {
+  run amazoned.sh();
+}
+# I an very new to Bash, so these might come out as script kiddie like
+
+"""
+File info
+
+File version: 1 (Tuesday, June 22nd 2020 at 12:34 pm)
+
+Line count: 35
+
+File type: BASH Shell script (*.sh)
+"""
+
+# End of script
