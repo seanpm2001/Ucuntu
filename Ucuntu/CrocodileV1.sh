279fe2571864bcdfbe053b7fb22e1d5cf4c58e6d
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 15:31:57 -0700

    CrocodileV1.sh
    An archive of the first version of the Crocodile shell script for destroying snap packages.

---
+++ Ucuntu/CrocodileV1.sh
@@ -0,0 +1,16 @@
+# Start of script
+"""
+Info
+Crocodile.sh might be the only file that isn't meant to annoy users. Its purpose is to break Ubuntu snap packages.
+"""
+
+# I don't know how to make something like this yet
+
+"""
+File info
+File version: 1 (Tuesday, June 23rd 2020 at 3:31 pm)
+Line count (including spaces): 17
+File type: BASH shell script (*.sh)
+"""
+
+# End of script
