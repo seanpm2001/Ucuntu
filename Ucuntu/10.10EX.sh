ff6b4c9f451d035b31f086ce5c0c6430a0807502
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 15:17:02 -0700

    10.10EX.sh
    The external script for the 10.10 joke payload.

---
+++ Ucuntu/10.10EX.sh
@@ -0,0 +1,19 @@
+# Start of script
+"""
+Info
+The 10.10 External file is a file that activates on October 10th 2010 at 10:10 pm (the release of Ubuntu 10.10, but on the opposite side of the day)
+It begins writing an infinite amount of 10.10s to the default terminal. There are 4 variations of this script
+"""
+
+$ echo "10.10_10.10_" -c 2000000 > 10.10.txt; # This will write 10.10_10.10_ 2000000 times, using up 24 GB of space (if it runs, I feel like this wasn't written correctly. 
+
+# I am not very good with Bash Shell, so this may be a scriptKiddie program
+
+"""
+File info
+File version: 1 (Tuesday, June 23rd 2020 at 3:17 pm)
+Line count (including spaces): 20
+File type: Bash Shell script (*.sh)
+"""
+
+# End of script
