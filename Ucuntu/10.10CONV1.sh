15a20fe0d54434128dba127b4f85a078a6f64ef1
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 15:29:03 -0700

    10.10CONV1.sh
    An archive of the first version of the 10.10 console script.

---
+++ Ucuntu/10.10CONV1.sh
@@ -0,0 +1,27 @@
+# Start of script
+"""
+Info
+The 10.10 Console file is a file that activates on October 10th 2010 at 10:10 am (the release of Ubuntu 10.10)
+It begins writing an finite amount of 10.10s to the default terminal. There are 4 variations of this script
+"""
+
+# Main method
+more 10.10.txt
+until footer {
+  more 10.10.txt
+}
+more10.10.txt
+
+# Backup method
+open 10.10.txt # Opens the External file in the console/terminal
+
+# I am not very good with Bash Shell, so this may be a scriptKiddie program
+
+"""
+File info
+File version: 1 (Tuesday, June 23rd 2020 at 3:28 pm)
+Line count (including spaces): 28
+File type: Bash Shell script (*.sh)
+"""
+
+# End of script
