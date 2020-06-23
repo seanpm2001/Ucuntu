38b77ba17fecc4e2ca0aee5cf9d54933ac14ca52
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 15:19:59 -0700

    1010EX_V1.sh
    An archive of the first version of 1010 external file shell script.

---
+++ Ucuntu/1010EX_V1.sh
@@ -0,0 +1,19 @@
+# Start of script
+"""
+Info
+The 1010 External file is a file that activates on October 10th 2010 at 10:10 am (the release of Ubuntu 10.10)
+It begins writing an finite amount of 10s to the default terminal. There are 4 variations of this script
+"""
+
+$ echo "101010101010" -c 2000000 > 1010.txt; # This will write 101010101010 2000000 times, using up 24 GB of space (if it runs, I feel like this wasn't written correctly. 
+
+# I am not very good with Bash Shell, so this may be a scriptKiddie program
+
+"""
+File info
+File version: 1 (Tuesday, June 23rd 2020 at 3:19 pm)
+Line count (including spaces): 20
+File type: Bash Shell script (*.sh)
+"""
+
+# End of script
